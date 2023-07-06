4bf356a58a44e2b34348abfc6d4a3c8e699e17ff
wang han <wh77534673@163.com>
2023-07-06 09:46:27 +0800

    2153398_wh_sign1.2
    再次修改，实现了判断“输入密码”和“确认密码”的判断逻辑，在注册页面也添加了“确认密码”

--- Frontend/src/components/recover.vue
+++ Frontend/src/components/recover.vue
@@ -6,13 +6,14 @@
     <input type="text" id="securityAns" v-model="securityAns" required>
     <br>
     <label for="newPword">输入新密码：</label>
-    <input type="password" id="newPword" v-model="newPword" :type="passwordVisible ? 'text' : 'password'"
+    <input id="newPword" v-model="newPword" :type="passwordVisible ? 'text' : 'password'"
       required>
-    <button @click="ShoworHide">{{ passwordVisible ? '隐藏密码' : '显示密码' }}</button>
     <br>
     <label for="confirmPword">确认新密码：</label>
-    <input type="password" id="confirmPword" v-model="confirmPword" :type="passwordVisible ? 'text' : 'password'"
+    <input id="confirmPword" v-model="confirmPword" :type="passwordVisible ? 'text' : 'password'"
       required>
+    <br>
+    <button @click="ShoworHide">{{ passwordVisible ? '隐藏密码' : '显示密码' }}</button>
     <br>
     <button @click="recoverPword">确认修改</button>
   </div>
@@ -32,8 +33,13 @@ export default {
   methods: {
     ShoworHide() {
       this.passwordVisible = !this.passwordVisible;
+      console.log(passwordVisible);
     },
     recoverPword() {
+      if(this.newPword !== this.confirmPword) {
+        alert('确认密码与新密码不一致');
+        return;
+      }
       // 在这里编写找回密码的逻辑，可以发送请求将密保答案和新密码提交到服务器进行验证和修改
       console.log('密保答案:', this.securityAns);
       console.log('新密码:', this.newPword);
