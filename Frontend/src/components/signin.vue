b120c4d29c14d0545f7e052f701a0e9211550246
DATE-W <517236597@qq.com>
2023-07-06 23:26:04 +0800

    2151935wyh_signin&carousel增加

--- Frontend/src/components/signin.vue
+++ Frontend/src/components/signin.vue
@@ -1,20 +1,4 @@
 <template>
-<<<<<<< Updated upstream
-  <div>
-    <h1>登录</h1>
-    <form @submit.prevent="register">
-      <label for="account">账号：</label>
-      <input type="text" id="account" v-model="account" pattern="[a-zA-Z0-9]+" required maxlength="10">
-      <p>账号只能由数字和字母组成，且长度不超过10个字符</p>
-      <br>
-      <label for="password">密码：</label>
-      <input type="password" id="password" v-model="password" pattern="[a-zA-Z0-9]+" required maxlength="15">
-      <p>密码只能由数字和字母组成，且长度不超过15个字符</p>
-      <br>
-      <button type="submit">登录</button>
-      <button @click="this.$router.push('/recover')">找回密码</button>
-    </form>
-=======
   <div class="common-layout">
     <el-container>
       <el-aside style="width: 100vh;">
@@ -53,43 +37,28 @@
         <my-carousel></my-carousel>
       </el-main>
     </el-container>
->>>>>>> Stashed changes
   </div>
 </template>
 
 <script>
-<<<<<<< Updated upstream
-=======
 import carousel from './signinCarousel.vue';
 
->>>>>>> Stashed changes
 export default {
   data() {
     return {
       account: '',
       password: '',
-<<<<<<< Updated upstream
-    };
-  },
-=======
       showPassword: false,
     };
   },
   components: {
       'my-carousel': carousel
   },
->>>>>>> Stashed changes
   methods: {
     register() {
       // 在这里编写注册逻辑，可以发送请求将账号和密码提交到服务器
       console.log('账号:', this.account);
       console.log('密码:', this.password);
-<<<<<<< Updated upstream
-    }
-  }
-};
-</script>
-=======
     },
     redirectToRegister() {
       // 跳转到注册页面的逻辑
@@ -232,4 +201,3 @@ export default {
   height: 100%;
   object-fit: contain;
 }</style>
->>>>>>> Stashed changes
