331b668fe2727ee91d59578711e41f8e8843831b
wang han <wh77534673@163.com>
2023-07-06 09:14:13 +0800

    2153398_wh_sign
    添加了登录、注册、找回密码的基础内容(设置密保  无CSS  无后端交互)

---
+++ Frontend/src/components/recover.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+      <h1>找回密码</h1>
+      <p>{{ securityQ }}</p>
+      <label for="securityAns">密保答案：</label>
+      <input type="text" id="securityAns" v-model="securityAns" required>
+      <br>
+      <label for="newPword">输入新密码：</label>
+      <input type="password" id="newPword" v-model="newPword" :type="passwordVisible ? 'text' : 'password'"
+        required>
+      <button @click="show_or_hide">{{ passwordVisible ? '隐藏密码' : '显示密码' }}</button>
+      <br>
+      <label for="confirmPword">确认新密码：</label>
+      <input type="password" id="confirmPword" v-model="confirmPword" :type="passwordVisible ? 'text' : 'password'"
+        required>
+      <button @click="show_or_hide">{{ passwordVisible ? '隐藏密码' : '显示密码' }}</button>
+      <br>
+      <button @click="recoverPword">确认修改</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        securityQ: '密保问题', // 替换为实际的密保问题
+        securityAns: '',
+        newPword: '',
+        confirmPword: '',
+        passwordVisible: false
+      };
+    },
+    methods: {
+      show_or_hide() {
+        this.passwordVisible = !this.passwordVisible;
+      },
+      recoverPword() {
+        // 在这里编写找回密码的逻辑，可以发送请求将密保答案和新密码提交到服务器进行验证和修改
+        console.log('密保答案:', this.securityAns);
+        console.log('新密码:', this.newPword);
+        console.log('确认密码:', this.confirmPword);
+      }
+    }
+  };
+  </script>
+  
--- Frontend/src/components/signup.vue
+++ Frontend/src/components/signup.vue
@@ -1,3 +1,49 @@
 <template>
-    <p>注册</p>
-</template>
+    <div>
+      <h1>注册</h1>
+      <form @submit.prevent="register">
+        <label for="account">账号：</label>
+        <input type="text" id="account" v-model="account" pattern="[a-zA-Z0-9]+" required maxlength="10">
+        <p>账号只能由数字和字母组成，且长度不超过10个字符</p>
+        <br>
+        <label for="password">密码：</label>
+        <input type="password" id="password" v-model="password" pattern="[a-zA-Z0-9]+" required maxlength="15">
+        <p>密码只能由数字和字母组成，且长度不超过15个字符</p>
+        <br>
+        <label for="securityQ">密保问题：</label>
+        <input type="text" id="securityQ" v-model="securityQ" pattern="[\u4e00-\u9fa5\d\s\p{P}]+"
+          required maxlength="10">
+        <p>密保问题只能包含中文、数字和标点符号，且长度不超过10个字符</p>
+        <br>
+        <label for="securityAns">密保答案：</label>
+        <input type="text" id="securityAns" v-model="securityAns" pattern="[\u4e00-\u9fa5\d\s\p{P}]+"
+          required maxlength="10">
+        <p>密保答案只能包含中文、数字和标点符号，且长度不超过10个字符</p>
+        <br>
+        <button type="submit">注册</button>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        account: '',
+        password: '',
+        securityQ: '',
+        securityAns: ''
+      };
+    },
+    methods: {
+      register() {
+        // 在这里编写注册逻辑，可以发送请求将账号、密码、密保问题和密保答案提交到服务器
+        console.log('账号:', this.account);
+        console.log('密码:', this.password);
+        console.log('密保问题:', this.securityQ);
+        console.log('密保答案:', this.securityAns);
+      }
+    }
+  };
+  </script>
+  
