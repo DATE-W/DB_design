47ef3c04e4fba4cd0250b93f6999db53734b99dc
Xie_yi <103160912+HeartFaker@users.noreply.github.com>
2023-07-06 13:52:11 +0800

    2153061xjq 注册v1.2
    添加logo，修改输入框，测试分支切换问题

--- Frontend/src/components/signup.vue
+++ Frontend/src/components/signup.vue
@@ -1,3 +1,216 @@
+<script>
+export default {
+  data() {
+    return {
+      account: '',
+      password: '',
+      securityQ: '',
+      securityAns: ''
+    };
+  },
+  methods: {
+    register() {
+      if(this.password !== this.confirmpassword) {
+      alert('确认密码与新密码不一致');
+      return;
+    }
+      // 在这里编写注册逻辑，可以发送请求将账号、密码、密保问题和密保答案提交到服务器
+      console.log('账号:', this.account);
+      console.log('密码:', this.password);
+      console.log('密保问题:', this.securityQ);
+      console.log('密保答案:', this.securityAns);
+    }
+  }
+};
+</script>
+
 <template>
-    <p>注册</p>
+  <div class="common-layout">
+    <el-container>
+        <el-aside>
+            <img src="../assets/img/football_logo.jpg" class="logoBox">
+            <text class="footGameTypography">FootGame</text>
+        </el-aside>
+        <el-main>
+            <!-- 注册字体 -->
+            <div class="FontLayout">
+            <p class="FontTypography">注册</p>
+            </div>
+            <!-- 输入内容 -->
+            <form @submit.prevent="register">
+            <!-- 输入账号 -->
+            <div class="subBox" style="top:170px;">
+                <label for="account" class="subTittle subTittleTypography" >账号：</label>
+                <input 
+                type="text" 
+                id="account" 
+                v-model="account" 
+                pattern="[a-zA-Z0-9]+" 
+                required maxlength="10" 
+                class="inputBox inputTypography"
+                >
+                <p class="holderTypography">账号只能由数字和字母组成，且长度不超过10个字符</p>
+            </div>
+            <!-- 输入密码 -->
+            <div class="subBox" style="top:290px;">
+                <label for="password" class="subTittle subTittleTypography" >密码：</label>
+                <input 
+                type="password" 
+                id="password" 
+                v-model="password" 
+                pattern="[a-zA-Z0-9]+" 
+                required maxlength="15"
+                class="inputBox inputTypography"
+                >
+                <p class="holderTypography">密码只能由数字和字母组成，且长度不超过15个字符</p>
+            </div>
+            <!-- 输入密码 -->
+            <div class="subBox" style="top:390px;">
+                <label for="confirmpassword" class="subTittle subTittleTypography">确认密码：</label>
+                <input 
+                type="password" 
+                id="confirmpassword" 
+                v-model="confirmpassword" 
+                pattern="[a-zA-Z0-9]+" 
+                required maxlength="15"
+                class="inputBox inputTypography"
+                >
+                <p class="holderTypography">请再次输入密码</p>
+            </div>
+            <!-- 密保问题 -->
+            <div class="subBox" style="top:490px;">
+                <label for="securityQ" class="subTittle subTittleTypography">密保问题：</label>
+                <input 
+                type="text" 
+                id="securityQ" 
+                v-model="securityQ" 
+                pattern="[\u4e00-\u9fa5\d\s\p{P}]+"
+                required maxlength="10"
+                class="inputBox inputTypography"
+                >
+                <p class="holderTypography">密保问题只能包含中文、数字和标点符号，且长度不超过10个字符</p>
+            </div>
+            <!-- 密保答案 -->
+            <div class="subBox" style="top:590px;">
+                <label for="securityAns" class="subTittle subTittleTypography">密保答案：</label>
+                <input 
+                type="text" 
+                id="securityAns" 
+                v-model="securityAns" 
+                pattern="[\u4e00-\u9fa5\d\s\p{P}]+"
+                required maxlength="10"
+                class="inputBox inputTypography"
+                >
+                <p class="holderTypography">密保答案只能包含中文、数字和标点符号，且长度不超过10个字符</p>
+            </div>
+            <!-- 注册按钮
+            <button type="submit">注册</button> -->
+            </form>
+        </el-main>
+    </el-container>
+  </div>
 </template>
+
+<style scoped>
+.el-aside{
+    position: absolute;
+    background: linear-gradient(180deg, #77B0FE 0%, rgba(119, 176, 254, 0.10) 100%);
+    top:0;
+    left:0;
+    width:50%;
+    height:100%;
+}
+/*左上角图标＋文字的style*/
+.logoBox{
+    position: absolute;
+    top:73px;
+    left:88px;
+    width:40px;
+    height:40px;
+}
+.footGameTypography{
+    position: absolute;
+    top:64px;
+    left:138px;
+    color: #000;
+    font-family: Poppins;
+    font-size: 36px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 163%;
+    letter-spacing: 5.4px;
+}
+
+/*文字“注册”的style*/
+.FontLayout{
+    display: flex;
+    position:absolute;
+    width: 134px;
+    height: 89px;
+    top:0px;
+    right:144px;
+    flex-direction: column;
+    flex-shrink: 0;
+}
+.FontTypography{
+    color: #3E4772;
+    font-family: Poppins;
+    font-size: 50px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+}
+/*用来装入每一个子标题+输入框的box*/
+.subBox{
+    position:absolute;
+    right:100px;
+    width: 600px;
+    height: 70px;
+    flex-shrink: 0;
+}
+/*每一个子标题的位置及样式*/
+.subTittle{
+    display: flex;
+    position: relative;
+    left:150px;
+    width: 100px;
+    height: 44.917px;
+    flex-direction: column;
+    flex-shrink: 0;
+    text-align: left;
+}
+.subTittleTypography{
+    color: #000;
+    font-family: Poppins;
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+}
+/*每一个输入框的样式及内置提示*/
+.inputBox{
+    border: 0;
+    position: absolute;
+    left: 150px;
+    width: 400px;
+    height: 30px;
+    flex-shrink: 0;
+    text-align: center;
+}
+.inputTypography{
+    border-radius: 30px;
+    background: #F1F1F1;
+}
+.holderTypography{
+    position: relative;
+    top: 40px;
+    left:170px;
+    color:black;
+    font-family: Poppins;
+    font-size: 10px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+}
+</style>
+  
