a8b1e1a3dc54d9e096c42a3a8b497487d17b3803
ReoCen <1298676106@qq.com>
2023-07-07 16:11:53 +0800

    2151784-wrb-登录注册忘记密码统一前端样式2.0
    对登录注册忘记密码三个界面进行统一，使其样式一致，并对原页面进行一定程度的优化。

--- Frontend/src/components/recover.vue
+++ Frontend/src/components/recover.vue
@@ -1,40 +1,50 @@
 <template>
   <div class="common-layout">
     <el-container>
+      <!-- 左侧走马灯 -->
       <el-aside width="50%" class="leftBg">
         <my-carousel></my-carousel>
       </el-aside>
+      <!-- 右侧输入框 -->
       <el-main>
+
         <div class="right">
+          <!-- 右侧左上角logo -->
           <img src="../assets/img/recover_logo.png" class="imgR">
-        <div class="mainbox">
-          
-          <el-space direction="vertical">
-            <h1 class="labelH1">Reset password to retrieve it</h1>    
-            <el-text tag="i" style="font-size: 15px;">Answer security questions and retrieve password</el-text>
-          </el-space>
-          <div>
-           <label for="securityAns" class="inputtext" style="left: -182px;">密保问题：&nbsp;&nbsp;&nbsp;</label>
-           <p >{{ securityQ }}</p>
-           <label for="securityAns" class="inputtext" style="left: -182px;">密保答案：&nbsp;&nbsp;&nbsp;</label>
-           <el-input type="text" id="securityAns" v-model="securityAns" required  class="inputbox"/>
-
-           <br>
-              <label for="newPword" class="inputtext" style="left: -180px;">输入新密码：</label>
-              <el-input  type="password" v-model="newPword" show-password class="inputbox"/>
-            <br>
-              <label for="confirmPword" class="inputtext" style="left: -180px;">确认新密码：</label>
-              <el-input id="confirmPword" v-model="confirmPword"  show-password required  class="inputbox"/>
+          <!-- 右侧总框夹 -->
+          <div class="mainbox">
+            <!-- 找回密码标题 -->
+            <el-space direction="vertical">
+              <h1 class="labelH1">Reset password to retrieve it</h1>
+              <el-text tag="i" style="font-size: 0.9rem;">Answer security questions and retrieve password</el-text>
+            </el-space>
+            <div>
+              <!-- 密保问题 -->
+              <label for="securityAns" class="inputtext" style="left: -10.5vw;">密保问题：&nbsp;&nbsp;&nbsp;</label>
+              <p>{{ securityQ }}</p>
+              <!-- 密保答案 -->
+              <label for="securityAns" class="inputtext" style="left: -10.5vw;">密保答案：&nbsp;&nbsp;&nbsp;</label>
+              <el-input type="text" id="securityAns" v-model="securityAns" required class="inputbox" />
+              <br>
+              <!-- 新密码 -->
+              <label for="newPword" class="inputtext" style="left: -10.4vw;">输入新密码：</label>
+              <el-input type="password" v-model="newPword" show-password class="inputbox" />
+              <br>
+              <!-- 确认密码 -->
+              <label for="confirmPword" class="inputtext" style="left: -10.4vw;">确认新密码：</label>
+              <el-input id="confirmPword" v-model="confirmPword" show-password required class="inputbox" />
+              <!-- 温馨提示 -->
               <el-text tag="i" class="labeltext">Please remember your password!</el-text>
-            <br>&nbsp;
-            <br>
+              <br>&nbsp;
+              <br>
+              <!-- 确认按钮 -->
               <button @click="recoverPword" class="button">确认修改</button>
+            </div>
+
           </div>
-          
-        </div>
 
         </div>
-        
+
       </el-main>
     </el-container>
   </div>
@@ -54,7 +64,7 @@ export default {
     };
   },
   components: {
-      'my-carousel': carousel
+    'my-carousel': carousel
   },
   methods: {
     ShoworHide() {
@@ -62,7 +72,7 @@ export default {
       console.log(passwordVisible);
     },
     recoverPword() {
-      if(this.newPword !== this.confirmPword) {
+      if (this.newPword !== this.confirmPword) {
         alert('确认密码与新密码不一致');
         return;
       }
@@ -76,120 +86,118 @@ export default {
 </script>
 
 <style scoped>
-.labelH1{
-  color: #525252;font-family: Nunito Sans;font-size: 30px;font-style: normal;font-weight: 700;line-height: normal;
+/*注册标题*/
+.labelH1 {
+  color: #525252;
+  font-family: Nunito Sans;
+  font-size: 1.8rem;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
 }
 
-  .common-layout {
-    height: 100vh; /* 设置容器高度为视口高度 */
-    width: 100%; /* 设置容器宽度为100% */
-  }
+.common-layout {
+  height: 100vh;
+  /* 设置容器高度为视口高度 */
+  width: 100%;
+  /* 设置容器宽度为100% */
+}
 
-  .el-container {
-    height: 100%; /* 设置 el-container 元素占满容器高度 */
-    width: 100%; /* 设置 el-container 元素占满容器宽度 */
-  }
+.el-container {
+  height: 100%;
+  /* 设置 el-container 元素占满容器高度 */
+  width: 100%;
+  /* 设置 el-container 元素占满容器宽度 */
+}
 
-  .leftBg{
-    /* background: linear-gradient(180deg, #77B0FE 0%, rgba(119, 176, 254, 0.10) 100%); */
-    background: linear-gradient(180deg, rgba(49, 49, 49, 0.30) 0%, rgba(0, 0, 0, 0.55) 100%), url(../assets/img/recover.png), lightgray 50% / cover no-repeat;
-  }
-  .leftLogo1{
-    top: 120px;
-    left:50px;
-    width: 60px;
-    height: 60px;
-    flex-shrink: 0;
-    fill: #ffffff;
-    position: relative;
-  }
-  .leftLogo2{
-    color: #ffffff;
-    font-family: Poppins;
-    font-size: 50px;
-    font-style: normal;
-    font-weight: 600;
-    line-height: 163%;
-    letter-spacing: 10px;
-    position: relative;
-    left:160px;
-  }
+/*左侧背景图片 */
+.leftBg {
+  /* background: linear-gradient(180deg, #77B0FE 0%, rgba(119, 176, 254, 0.10) 100%); */
+  background: linear-gradient(180deg, rgba(49, 49, 49, 0.30) 0%, rgba(0, 0, 0, 0.55) 100%), url(../assets/img/recover.png), lightgray 50% / cover no-repeat;
+}
 
-  .imgR{
-    top:80px;
-    left: -250px;
-    
-    position: relative;
-  }
+/*右侧左上logo样式 */
+.imgR {
+  top: 8vh;
+  left: -16vw;
+  width: 4vw;
+  height: 7vh;
+  position: relative;
+}
 
-  .right{
-    text-align:center;
-    vertical-align: middle;
-    line-height:1;
-  }
+/*右侧总览 */
+.right {
+  text-align: center;
+  vertical-align: middle;
+  line-height: 1;
+}
 
-  .mainbox{
-    top:130px;
-    left:200px;
-    position: relative;
-    display: flex;
-    width: 450px;
-    height: 450px;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    gap: 36px;
-    flex-shrink: 0;  
-    /*background-color: antiquewhite;*/
-  }
+/*右侧输入总框 */
+.mainbox {
+  top: 16vh;
+  left: 11vw;
+  position: relative;
+  display: flex;
+  width: 30vw;
+  height: 45vh;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 2.21rem;
+  flex-shrink: 0;
+  /*background-color: antiquewhite;*/
+}
 
-  .inputtext{
-    position: relative;
-    color: var(--gray-3, #828282);
-    font-family: Nunito Sans;
-    font-size: 14px;
-    font-style: normal;
-    font-weight: 600;
-    line-height: normal;
-    bottom: 5px;
-  }
+/*输入提示标题 */
+.inputtext {
+  position: relative;
+  color: var(--gray-3, #828282);
+  font-family: Nunito Sans;
+  font-size: 0.85rem;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  bottom: 0.0001vh;
+}
 
-  .inputbox{
-    position: relative;
-    display: flex;
-    width: 420px;
-    padding: 13px 10px;
-    align-items: center;
-    gap: 13px;
-    border-radius: 5px;
-    border: 0;
-  }
+/*输入框 */
+.inputbox {
+  position: relative;
+  display: flex;
+  width: 26.5vw;
+  padding: 1.3vh 0.6vw;
+  align-items: center;
+  gap: 0.78rem;
+  border-radius: 0.31rem;
+  border: 0;
+}
 
-  .button{
-    display: flex;
-    width: 420px;
-    padding: 13px 10px 12px 10px;
-    justify-content: center;
-    align-items: center;
-    gap: 13px;
-    border-radius: 6px;
-    background: #007DFA;
-    color: #FFF;
-    font-family: Nunito Sans;
-    font-size: 18px;
-    font-style: normal;
-    font-weight: 800;
-    line-height: normal;
-    border: 0;
-  }
+/*确认按钮 */
+.button {
+  display: flex;
+  width: 26vw;
+  padding: 1.2vh 0.6vw 1.2vh 0.6vw;
+  justify-content: center;
+  align-items: center;
+  gap: 0.78rem;
+  border-radius: 0.31rem;
+  background: #007DFA;
+  color: #FFF;
+  font-family: Nunito Sans;
+  font-size: 1.1rem;
+  font-style: normal;
+  font-weight: 800;
+  line-height: normal;
+  border: 0;
+}
 
-  .labeltext{
-    color: #7F265B;
-    font-size: 10px;
-    position: relative;
-    left:130px;
-    top:0px;
-    bottom:10px;
-  }
-
+/*温馨提示样式*/
+.labeltext {
+  color: #7F265B;
+  font-size: 0.6rem;
+  position: relative;
+  left: 7.5vw;
+  top: 0;
+  bottom: 1vh;
+}
 </style>
--- Frontend/src/components/signup.vue
+++ Frontend/src/components/signup.vue
@@ -40,89 +40,93 @@ export default {
           <my-carousel />
         </el-aside>
         <el-main>
+          <img src="../assets/img/recover_logo.png" class="imgR">
             <!-- 注册 -->
-            <div style="height:10%">
-              <div class="FontLayout">
-                <p class="FontTypography">注册</p>
-              </div>
+            <div style="bottom:3vh">
+              <el-space direction="vertical" style="text-align:center">
+                <h1 class="labelH1 excenter">Register to create your account</h1>    
+                <el-text tag="i" class="excenter" style="font-size: 0.9rem;">Set password and account for creation</el-text>
+              </el-space>
             </div>
             <!-- 输入内容 -->
-            <div style="height:70%">
+            <div class="maininputbox">
               <form @submit.prevent="register">
                 <!-- 输入账号 -->
                 <div class="subBox">
-                    <label for="account" class="subTittle subTittleTypography" >账号：</label>
+                    <label for="account" class="inputText" left=10.3vw;>账号：</label>
                     <el-input 
                     type="text" 
                     id="account" 
                     v-model="account" 
                     pattern="[a-zA-Z0-9]+" 
                     required maxlength="10" 
-                    class="inputBox inputTypography"
+                    class="inputBox"
                     placeholder="账号只能由数字和字母组成，且长度不超过10个字符"
                     />
                 </div>
                 <!-- 输入密码 -->
                 <div class="subBox">
-                    <label for="password" class="subTittle subTittleTypography" >密码：</label>
+                    <label for="password" class="inputText" left=10.3vw;>密码：</label>
                     <el-input 
                     type="password" 
                     id="password" 
                     v-model="password" 
                     pattern="[a-zA-Z0-9]+" 
                     required maxlength="15"
-                    class="inputBox inputTypography"
+                    class="inputBox  "
                     show-password
                     placeholder="密码只能由数字和字母组成，且长度不超过15个字符"
                     />
                 </div>
                 <!-- 再次输入密码 -->
                 <div class="subBox">
-                    <label for="confirmpassword" class="subTittle subTittleTypography">确认密码：</label>
+                    <label for="confirmpassword" class="inputText">确认密码：</label>
                     <el-input 
                     type="password" 
                     id="confirmpassword" 
                     v-model="confirmpassword" 
                     pattern="[a-zA-Z0-9]+" 
                     required maxlength="15"
-                    class="inputBox inputTypography"
+                    class="inputBox  "
                     show-password
                     placeholder="请再次输入密码"
                     />
                 </div>
                 <!-- 密保问题 -->
                 <div class="subBox">
-                    <label for="securityQ" class="subTittle subTittleTypography">密保问题：</label>
+                    <label for="securityQ" class="inputText">密保问题：</label>
                     <el-input 
                     type="text" 
                     id="securityQ" 
                     v-model="securityQ" 
                     pattern="[\u4e00-\u9fa5\d\s\p{P}]+"
                     required maxlength="10"
-                    class="inputBox inputTypography"
+                    class="inputBox  "
                     placeholder="密保问题只能包含中文、数字和标点符号，且长度不超过10个字符"
                     />
                 </div>
                 <!-- 密保答案 -->
                 <div class="subBox">
-                    <label for="securityAns" class="subTittle subTittleTypography">密保答案：</label>
+                    <label for="securityAns" class="inputText">密保答案：</label>
                     <el-input 
                     type="text" 
                     id="securityAns" 
                     v-model="securityAns" 
                     pattern="[\u4e00-\u9fa5\d\s\p{P}]+"
                     required maxlength="10"
-                    class="inputBox inputTypography"
+                    class="inputBox  "
                     placeholder="密保答案只能包含中文、数字和标点符号，且长度不超过10个字符"
                     />
                 </div>
+                <div>
+                  <el-text tag="i" class="labeltext">Please remember your password and security！</el-text>
+                </div>
+                
               </form>
             </div>
             <!-- 注册按钮 -->
             <div>
-              <text @click="redirectToLogin" class="havePos haveText">
-                已经有账号了？
-              </text>
+              <el-text @click="redirectToLogin" tag="i" class="havePos haveText">已经有账号了？</el-text>
               <button type="submit" class="signupBtn">
                 <text class="btnText">注册</text>
               </button>
@@ -148,108 +152,50 @@ export default {
     width:50%;
     height:100%;
 }
-/*左上角图标＋文字的style*/
-.logoBox{
-    position: relative;
-    top:4vw;
-    left:8vw;
-    width:2vw;
-    height:2vw;
-}
-.footGameTypography{
-    position: relative;
-    top:3.8vw;
-    left:10vw;
-    color: #000;
-    font-family: Poppins;
-    font-size: 2rem;
-    font-style: normal;
-    font-weight: 600;
-    line-height: 163%;
-    letter-spacing: 0.54rem;
-}
 
-/*文字“注册”的style*/
-.FontLayout{
-    text-align: center;
-    position:relative; 
-    width: 13.4vw;
-    top:0;
-    height: 5vw;
-    left:40%;
-    flex-direction: column;
-    flex-shrink: 0;
-}
-.FontTypography{
-    color: #3E4772;
-    font-family: Poppins;
-    font-size: 2.5rem;
-    font-style: normal;
-    font-weight: 700;
-    line-height: normal;
-}
 /*用来装入每一个子标题+输入框的box*/
 .subBox{
     position:relative;
-    right:10vw;
     left:5%;
     width: 25vw;
-    height: 7vw;
+    height: 5vw;
     flex-shrink: 0;
 }
 /*每一个子标题的位置及样式*/
-.subTittle{
-    display: flex;
-    position: relative;
-    left:10vw;
-    width: 10vw;
-    height: 2.5vw;
-    flex-direction: column;
-    flex-shrink: 0;
-    text-align: left;
-}
-.subTittleTypography{
-    color: #000;
-    font-family: Poppins;
-    font-size: 1.2rem;
-    font-style: normal;
-    font-weight: 400;
-    line-height: normal;
+.inputText{
+  left:6vw;
+  position: relative;
+  color: var(--gray-3, #828282);
+  font-family: Nunito Sans;
+  font-size: 0.85rem;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
 }
 /*每一个输入框的样式及内置提示*/
 .inputBox{
+  position: relative;
+  left:5vw;
+    display: flex;
+    width: 33vw;
+    padding: 0.5vh 0.6vw;
+    align-items: center;
+    gap: 0;
+    border-radius: 0.31rem;
     border: 0;
-    position: relative;
-    left: 9vw;
-    width: 30vw;
-    height: 2vw;
-    flex-shrink: 0;
-    text-align: center;
 }
-.inputTypography{
-    border-radius: 3vw;
-    background: #F1F1F1;
-}
-.holderTypography{
-    position: relative;
-    left:10vw;
-    color:black;
-    font-family: Poppins;
-    font-size: 0.75rem;
-    font-style: normal;
-    font-weight: 400;
-    line-height: normal;
-}
+
 /* 注册按钮＋已经有账号了 */
 .signupBtn{
   position: relative;
-  left:45%;
-  width: 13vw;
-  height: 2.5vw;
-  bottom: -4vw;
+  left:1.5vw;
+  width: 30vw;
+  height: 2.6vw;
+  top:10vh;
   flex-shrink: 0;
-  border-radius: 3vw;
-  background: #2E375D;
+  border-radius: 0.31rem;
+  background: #007DFA;
+  border: 0;
 }
 .btnText{
   color: #FFF;
@@ -261,19 +207,63 @@ export default {
 }
 .havePos{
   position: relative;
-  left:43%;
+  left:67%;
   width: 0.5vw;
   height: 2.5vw;
-  bottom: -4vw;
+  top:15vh;
 }
 .haveText{
   cursor: pointer;
   color: #494F7A;
-  font-family: Poppins;
+  font-family: Nunito Sans;
   font-size: 1rem;
   font-style: normal;
   font-weight: 400;
   line-height: normal;
 }
-</style>
+
+/*右侧左上角logo*/ 
+.imgR{
+    top:8vh;
+    left: 4vw;
+    width:4vw;
+    height: 7vh;
+    position: relative;
+  }
+
+/*注册标题 */
+.labelH1{
+    color: #525252;
+    font-family: Nunito Sans;
+    font-size: 1.8rem;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+}
+
+/*右侧标题位置设置 */
+.excenter{
+  text-align:center;
+  line-height:1;
+  position: relative;
+  top:7vh;
+  left:9.5vw;
+}
+
+/*右侧输出框控制 */
+.maininputbox{
+  position: relative;
+  top:9vh;
+
+}
+
+.labeltext{
+  color: #7F265B;
+    font-size: 0.6rem;
+    position: relative;
+    left:23vw;
+    top:-2vh;
+    bottom:1vh;
+}
   
+</style>
