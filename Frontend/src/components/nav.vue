88017268d42bf0bf740e395bcea4914e999f52f8
litianlololo <102678203+litianlololo@users.noreply.github.com>
2023-07-07 18:18:41 +0800

    2151646李天昊 main0.4
    加入新闻预览 实现文字截断

--- Frontend/src/components/nav.vue
+++ Frontend/src/components/nav.vue
@@ -17,7 +17,7 @@
         <div class="nav-right">
         <el-dropdown trigger="hover">
           <span class="user-avatar" @click="redirectToPersonal">
-            <img src="avatar.jpg" alt="Avatar" />
+            <img src="../assets/img/football_logo.png" alt="Avatar" />
           </span>
           <template #dropdown>
             <el-dropdown-menu v-slot: dropdown>
