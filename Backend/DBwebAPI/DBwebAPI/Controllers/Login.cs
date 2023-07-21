c225d7e376bb504435ce2755ae89a70bb0a4c2ee
litianlololo <ajxflth@163.com>
2023-07-21 23:15:16 +0800

    后端登录注册完整版

--- Backend/DBwebAPI/DBwebAPI/Controllers/Login.cs
+++ Backend/DBwebAPI/DBwebAPI/Controllers/Login.cs
@@ -20,11 +20,11 @@ namespace DBwebAPI.Controllers
             Console.WriteLine("Conn success!");
 
             //提取参数
-            var jsonStr = JsonConvert.SerializeObject(json);
-            var jsonParams = JsonConvert.DeserializeObject<dynamic>(jsonStr);
-            string username = jsonParams["username"];
-            string passwordHash = jsonParams["passwordHash"];
-            Console.WriteLine("json get success!");
+
+            string account = json.Account;
+            string passwordHash = json.Password;
+            Console.WriteLine("account="+ account);
+            Console.WriteLine("passwordHash= "+passwordHash);
             //string securityQ = jsonParams["securityQ"];
             //string securityAnsHash = jsonParams["securityAnsHash"];
 
@@ -34,30 +34,32 @@ namespace DBwebAPI.Controllers
                     SqlSugarClient sqlORM = ORACLEConnectTry.sqlORM;
                     //进行用户查询
                     List<Usr> tempUsr = new List<Usr>();
-                    tempUsr = await sqlORM.Queryable<Usr>().Where(it => it.userName == username 
+                    tempUsr = await sqlORM.Queryable<Usr>().Where(it => it.userName == account
                     && it.userPassword == passwordHash)
                         .ToListAsync();
                     //判断用户是否存在
                     if (tempUsr.Count() == 0)
                     {
-                        return "error code=1";//用户账户或密码错误
+                        Console.WriteLine("登录失败");
+                        return "Fail";//用户账户或密码错误
                     }
                     else
                     {
+                        Console.WriteLine("登录成功");
                         createToken tempToken= new createToken();
-                        string token = tempToken.createTokenFun(username, passwordHash);
+                        string token = tempToken.createTokenFun(account, passwordHash);
                         return token;
                     }
 
                 }
                 catch (Exception)
                 {
-                    return "error code=114514";//位置错误
+                    return "UNKNOWN";//位置错误
                 }
             }
             else
             {
-                return "wyh是大傻逼";//连接数据库失败
+                return "Fail_Access";//连接数据库失败
             }
 
         }
--- Backend/DBwebAPI/DBwebAPI/Controllers/Register.cs
+++ Backend/DBwebAPI/DBwebAPI/Controllers/Register.cs
@@ -20,6 +20,7 @@ namespace DBwebAPI.Controllers
         [HttpPost]
         public async Task<string> normalRegisterAsync([FromBody] RegisterRequest registerRequest)
         {
+            Console.WriteLine("GET Register!");
             ORACLEconn ORACLEConnectTry = new ORACLEconn();
             if (ORACLEConnectTry.getConn() == true)
             {
@@ -37,25 +38,35 @@ namespace DBwebAPI.Controllers
                     usr.userSecQue = registerRequest.UserSecQue;
                     usr.userSecAns = registerRequest.UserSecAns;
                     usr.createDateTime = DateTime.Now;
+                    Console.WriteLine("user_id= "+ user_id);
+                    Console.WriteLine("userName= "+ usr.userName);
+                    Console.WriteLine("userPassword= " + usr.userPassword);
+                    Console.WriteLine("userAccount= "+ usr.userAccount);
+                    Console.WriteLine("userSecQue= "+ usr.userSecQue);
+                    Console.WriteLine("userSecAns= " + usr.userSecAns);
+                    Console.WriteLine("createDateTime= " + usr.createDateTime);
                     int count = await sqlOrm.Insertable(usr).ExecuteCommandAsync();
                     if (count == 1)
                     {
+                        Console.WriteLine("注册成功");
                         return "Success";
                     }
                     else
                     {
-                        return "error code=2";//用户注册的账户已存在
+                        Console.WriteLine("注册失败");
+                        return "Fail";//用户注册的账户已存在
                     }
                 }
                 catch (Exception ex)
                 {
+                    Console.WriteLine("未知错误");
                     System.Console.WriteLine(ex.Message);
-                    return "error code=114514";//未知错误
+                    return "UNKNOWN";//未知错误
                 }
             }
             else
             {
-                return "error code=0";//连接数据库失败
+                return "Fail_Access";//连接数据库失败
             }
         }
     }
