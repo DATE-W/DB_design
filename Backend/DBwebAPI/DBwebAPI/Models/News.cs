ef49ef4ecd20c1bef8ff0c5541a4116d05fa6cca
LevisT <1329361431@qq.com>
2023-08-07 23:35:10 +0800

    补上新闻接口的部分

---
+++ Backend/DBwebAPI/DBwebAPI/Models/News.cs
@@ -0,0 +1,14 @@
+﻿namespace DBwebAPI.Models
+{
+    public class News
+    {
+        public int news_id { get; set; }
+        public string title { get; set; }
+        public DateTime publishDateTime { get; set; }
+        public string summary { get; set; }
+        public string contains { get; set; }
+        public string matchTag { get; set; }
+        public string propertyTag { get; set; }
+        //public List<string>? pictureRoutes { get; set; }
+    }
+}
