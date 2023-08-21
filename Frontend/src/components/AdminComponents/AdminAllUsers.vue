261d001fa62c75e9629adc5913b38f8a91c6d363
Xie_yi <103160912+HeartFaker@users.noreply.github.com>
2023-08-21 11:26:30 +0800

    管理员界面v2.0 详情页update
    管理员界面渲染基本完成，详情页更新关注，举报功能

--- Frontend/src/components/AdminComponents/AdminAllUsers.vue
+++ Frontend/src/components/AdminComponents/AdminAllUsers.vue
@@ -5,7 +5,15 @@
     },
     data() {
         return {
-           
+            Users: [
+                { id: 1, name: 'RU 1' ,regDate:"2023-8-1",collectedNum:8,credits:10,hostTeam:"阿根廷"},
+                { id: 2, name: 'RU 2' ,regDate:"2023-8-1",collectedNum:9,credits:10,hostTeam:"阿根廷"},
+                { id: 3, name: 'RU 3' ,regDate:"2023-8-1",collectedNum:3,credits:10,hostTeam:"阿根廷"},
+                { id: 4, name: 'RU 4' ,regDate:"2023-8-1",collectedNum:4,credits:10,hostTeam:"阿根廷"},
+                { id: 5, name: 'RU 5' ,regDate:"2023-8-1",collectedNum:0,credits:10,hostTeam:"阿根廷"},
+                { id: 6, name: 'RU 6' ,regDate:"2023-8-1",collectedNum:6,credits:10,hostTeam:"阿根廷"},
+                { id: 7, name: 'RU 7' ,regDate:"2023-8-1",collectedNum:7,credits:10,hostTeam:"阿根廷"},
+            ],
         };
     },
     methods:{
@@ -16,7 +24,41 @@
 
 <template>
     <el-container class="main-lower-box">
-        所有用户
+        <el-table :data="Users" border height="300" style="width: 100%;border-radius: 10px;">
+            <el-table-row label="111"/>
+            <el-table-column align="center" prop="id" label="Id" width="100" />
+            <el-table-column prop="name" label="昵称" width="150" />
+            <el-table-column prop="regDate" label="注册时间" width="150" />
+            <el-table-column align="center" prop="collectedNum" label="总收藏数" width="100" />
+            <el-table-column align="center" prop="credits" label="积分" width="100" />
+            <el-table-column prop="hostTeam" label="主队" width="150" />
+            <el-table-column fixed="right" label="操作">
+                <template #default="scope">
+                    <el-popover
+                        placement="bottom"
+                        :width="200"
+                        trigger="click"
+                    >
+                        <span>{{Users[scope.$index].reason}}</span>
+                        <template #reference>
+                        <el-button link type="primary" size="small">私信用户</el-button>
+                        </template>
+                    </el-popover>
+                    <el-popconfirm
+                        width="220"
+                        confirm-button-text="确认"
+                        cancel-button-text="取消"
+                        :icon="InfoFilled"
+                        icon-color="#626AEF"
+                        title="确认删除此用户？"
+                    >
+                        <template #reference>
+                        <el-button link type="primary" size="small">删除用户</el-button>
+                        </template>
+                    </el-popconfirm>
+                </template>
+            </el-table-column>
+        </el-table>
     </el-container>
 </template>
 
