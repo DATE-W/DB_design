fdf6e8e1c08048ca5ac4b84c717587b2feee9429
kaizheng <996084341@qq.com>
2023-08-26 23:02:15 +0800

    features and fixes
    添加显示主队近期三场比赛功能，添加队标显示功能，美化页面；通过Uid来调用接口，实时渲染页面内容，增加各场比赛之间的直接跳转功能；修复了返回比赛一级页面时不能保留原有选择的bug，美化了队标显示。

--- Frontend/src/components/detailedMatch.vue
+++ Frontend/src/components/detailedMatch.vue
@@ -1,59 +1,61 @@
-<!-- 2154314_郑楷_赛事详情页 2023.08.11 14:00 v1.1.0 
-v1.0.0 添加页面，改路由，完成赛事列表向该页的UID传值 
-v1.1.0 向赛事列表主页面传值，使得之前的选择结果得到保存
-v1.2.0 画出基本页面功能，增加往球队详情页的跳转逻辑，增加往其他场次的跳转逻辑 -->
+<!-- 2154314_郑楷_赛事详情页 2023.08.26 14:00 v1.1.0 
+ v1.0.0 添加页面，改路由，完成赛事列表向该页的UID传值 
+ v1.1.0 向赛事列表主页面传值，使得之前的选择结果得到保存
+ v1.2.0 画出基本页面功能，增加往球队详情页的跳转逻辑，增加往其他场次的跳转逻辑
+ v1.3.0 通过Uid来调用接口，实时渲染页面内容，增加各场比赛之间的直接跳转功能
+ v1.4.0 修复了返回比赛一级页面时不能保留原有选择的bug，美化了队标显示 -->
 
 <template>
+  <my-nav></my-nav>
 
-<my-nav></my-nav>
+  <el-button size="large" type="info" icon="back" @click="toGames(this.dateChoice, this.lgeChoice)">Back</el-button>
 
-<el-button size="large" type="info" icon="back" @click="toGames(this.dateChoice,this.lgeChoice)">Back</el-button>
-
-<!-- <p>{{ gameUid }}</p> -->
+  <!-- <p>{{ gameUid }}</p> -->
 
 
-<div class="topTextBox" style="left: 35rem;top:5rem;">
-  <p class="textDate">{{ this.dateChoice }}&nbsp&nbsp{{ this.dateTime }}</p>
-  <p class="textStatus">{{ getMatchStatus(this.status) }}</p>
-  <p class="textLeague">{{ getLeagueInfo(this.leagueInfoNum) }}</p>
-</div>
+  <div class="topTextBox" style="left: 35rem;top:5rem;">
+    <p class="textDate">{{ this.match.dateTime }}&nbsp&nbsp{{ this.match.startTime }}</p>
+    <p class="textStatus">{{ getMatchStatus(this.match.status) }}</p>
+    <p class="textLeague">{{ getLeagueInfo(this.match.leagueType) }}</p>
+  </div>
 
-<div class="topTextBoxTRA" style="left: 13rem;top:5rem;width:20rem" @click="toTeams(this.homeTeamName)">
-  <p class="textTeamName" style="left:1rem">{{ this.homeTeamName }}</p>
-  <img class="picTeam" :src=this.homeLogo style="left:2rem">
-</div>
+  <div class="topTextBoxTRA" style="left: 13rem;top:5rem;width:20rem" @click="toTeams(this.match.homeTeamName)">
+    <p class="textTeamName" style="left:1rem">{{ this.match.homeTeamName }}</p>
+    <img class="picTeam" :src=this.match.homeLogo style="left:2rem">
+  </div>
 
-<div class="topTextBoxTRA" style="left: 64rem;top:5rem;width:20rem" @click="toTeams(this.guestTeamName)">
-  <p class="textTeamName" style="right:1rem">{{ this.guestTeamName }}</p>
-  <img class="picTeam" :src=this.guestLogo style="right:2rem">
-</div>
+  <div class="topTextBoxTRA" style="left: 64rem;top:5rem;width:20rem" @click="toTeams(this.match.guestTeamName)">
+    <p class="textTeamName" style="right:1rem">{{ this.match.guestTeamName }}</p>
+    <img class="picTeam" :src=this.match.guestLogo style="right:2rem">
+  </div>
 
-<div class="scoreBox" style="left: 31rem;top:8rem">
-  <p class="textScore">{{ this.homeScore }}</p>
-</div>
+  <div class="scoreBox" style="left: 31rem;top:8rem">
+    <p class="textScore">{{ scoreCheck(this.match.homeScore) }}</p>
+  </div>
 
-<div class="scoreBox" style="left: 61rem;top:8rem">
-  <p class="textScore">{{ this.guestScore }}</p>
-</div>
+  <div class="scoreBox" style="left: 61rem;top:8rem">
+    <p class="textScore">{{ scoreCheck(this.match.guestScore) }}</p>
+  </div>
 
-<p class="recentGameTypo" style="left:13rem">近期比赛</p>
+  <p class="recentGameTypo" style="left:13rem">近期比赛</p>
 
-<p class="recentGameTypo" style="left:79rem">近期比赛</p>
+  <p class="recentGameTypo" style="left:79rem">近期比赛</p>
 
-<div class="recentGameBoxLeft" v-for="(homeRecentGame,index) in homeRecentGames" :key="homeRecentGame.gameUid"
-:style="{ top: `${index * 7+22}rem` }" @click="toMatchDetail(homeRecentGame.gameUid,1,homeRecentGame.gameDate)" style="left:13rem;">
-  <p class="textMatchDate">{{ homeRecentGame.gameDate }}</p>
-  <p class="textMatchScore" style="left:5.5rem">{{ homeRecentGame.homeScore }}&nbsp:&nbsp{{ homeRecentGame.opponentScore }}</p>
-  <p class="textMatchOpp" style="right:0.5rem">{{ homeRecentGame.opponentName }}</p>
-</div>
+  <div class="recentGameBoxLeft" v-for="(homeRecentGame, index) in match.homeRecentGames" :key="homeRecentGame.gameUid"
+    :style="{ top: `${index * 7 + 22}rem` }" @click="this.getMatchByUid(homeRecentGame.gameUid);" style="left:13rem;">
+    <p class="textMatchDate">{{ homeRecentGame.gameDate }}</p>
+    <p class="textMatchScore" style="left:5.5rem">{{ homeRecentGame.homeScore }}&nbsp:&nbsp{{ homeRecentGame.opponentScore
+    }}</p>
+    <p class="textMatchOpp" style="right:0.5rem">{{ homeRecentGame.opponentName }}</p>
+  </div>
 
-<div class="recentGameBoxLeft" v-for="(guestRecentGame,index) in guestRecentGames" :key="guestRecentGame.gameUid"
-:style="{ top: `${index * 7+22}rem` }" @click="toMatchDetail(guestRecentGame.gameUid,1,guestRecentGame.gameDate)" style="left:64rem;">
-  <p class="textMatchDate" style="right:0">{{ guestRecentGame.gameDate }}</p>
-  <p class="textMatchScore" style="right:5.5rem">{{ guestRecentGame.opponentScore }}&nbsp:&nbsp{{ guestRecentGame.homeScore }}</p>
-  <p class="textMatchOpp" style="left:0.5rem">{{ guestRecentGame.opponentName }}</p>
-</div>
-
+  <div class="recentGameBoxLeft" v-for="(guestRecentGame, index) in match.guestRecentGames" :key="guestRecentGame.gameUid"
+    :style="{ top: `${index * 7 + 22}rem` }" @click="this.getMatchByUid(guestRecentGame.gameUid)" style="left:64rem;">
+    <p class="textMatchDate" style="right:0">{{ guestRecentGame.gameDate }}</p>
+    <p class="textMatchScore" style="right:5.5rem">{{ guestRecentGame.opponentScore }}&nbsp:&nbsp{{
+      guestRecentGame.homeScore }}</p>
+    <p class="textMatchOpp" style="left:0.5rem">{{ guestRecentGame.opponentName }}</p>
+  </div>
 </template>
 
 <script>
@@ -70,46 +72,77 @@ export default {
     this.gameUid = this.$route.query.gameUid;
     this.dateChoice = this.$route.query.dateChoice;
     this.lgeChoice = this.$route.query.lgeChoice;
+    console.log(this.gameUid);
+  },
+
+  mounted() {
+    this.getMatchByUid(this.gameUid);
   },
 
   methods: {
-    toGames(dateChoice,lgeChoice)
-    {
-        this.$router.push(
-        {
-          path:`/Games`,
-          query:{      
-            date11: dateChoice,
-            league11 :lgeChoice,   
-          }
-        }
-      );
+    // 调用接口获取赛事详情数据
+    async getMatchByUid(gameUidThis) {
+      let response
+      try {
+        response = await axios.post('/api/updateTeam/getGameByUid', {
+          gameUid: gameUidThis,
+        }, {})
+      } catch (err) {
+        ElMessage({
+          message: '获取赛事数据失败',
+          grouping: false,
+          type: 'error',
+        });
+      }
+      /* console.log(dateCho,leagueCho); */
+      /* this.homeTeamName = response.data.homeTeamName; */
+      this.match = response.data;
+      console.log(this.match);
     },
-    toTeams(teamName11)
-    {
+    // 比分空值处理
+    scoreCheck(score) {
+      if (!score) {
+        return 0;
+      }
+      else {
+        return score;
+      }
+    },
+    toGames(dateChoice, lgeChoice) {
       this.$router.push(
         {
-          path:`/teamMsg`,
-          query:{      
-            teamName: teamName11,  
+          path: `/Games`,
+          query: {
+            date11: dateChoice,
+            league11: lgeChoice,
           }
         }
       );
     },
-    toMatchDetail(uid,leagueC,dateC){
-      this.match11=uid;
+    toTeams(teamName11) {
+      this.$router.push(
+        {
+          path: `/teamMsg`,
+          query: {
+            teamName: teamName11,
+          }
+        }
+      );
+    },
+    toMatchDetail(uid, leagueC, dateC) {
+      this.match11 = uid;
       //etTimeout(function(){ getSignature() },5000);//Test
       this.$router.push(
         {
-          path:`/detailedMatch`,
-          query:{
+          path: `/detailedMatch`,
+          query: {
             gameUid: uid,
             lgeChoice: leagueC,
             dateChoice: dateC,
           }
         }
       );
-      
+
     },
     getLeagueInfo(status) {
       switch (status) {
@@ -133,36 +166,30 @@ export default {
           return "";
       }
     },
-    getMatchStatus(status){
+    getMatchStatus(status) {
       switch (status) {
-        case 0:
-          return "未开始";
-        case 1:
-          return "进行中";
-        case 2:
+        case 'Played':
           return "已结束";
+        case 'Unplayed':
+          return "未开始";
+        case 'Playing':
+          return "进行中";
+        case 'Postponed':
+          return "比赛延期";
         default:
-          return "";
+          return "待定";
       }
     },
   },
 
   data() {
     return {
-      lgeChoice:0,
-      dateChoice:ref(''),
+      lgeChoice: 0,
+      dateChoice: ref(''),
 
-      /* gameUid: ref(''),
-      dateTime: ref(''),
-      homeTeamName: ref(''),
-      guestTeamName: ref(''),
-      status: ref(0),
-      homeScore: ref(0),
-      guestScore: ref(0),
-      homeRecentGames: ref([]),
-      guestRecentGames: ref([]), */
+      match: ref([]),
 
-      gameUid: 'PRD13403419',
+      /* gameUid: 'PRD13403419',
       dateTime: '20:00',
       homeTeamName: '利物浦',
       guestTeamName: '曼彻斯特联',
@@ -182,163 +209,158 @@ export default {
         {"gameDate":"2020-03-10","opponentName":"皇马","homeScore":2,"opponentScore":3,"opponentLogo":"","gameUid":"TLQWY"}
 
       ],
-      leagueInfoNum: 1,
+      leagueInfoNum: 1, */
 
     }
-    
+
   }
 }
 </script>
 
 <style scoped>
 /* 框体风格 */
-.topTextBox
-{
-position: absolute;
-width: 27rem;
-height: 10rem;
-flex-shrink: 0; 
-background: white;
-border: 2px solid var(--colors-light-eaeaea-100, #EAEAEA);
-border-radius: 1rem;
+.topTextBox {
+  position: absolute;
+  width: 27rem;
+  height: 10rem;
+  flex-shrink: 0;
+  background: white;
+  border: 2px solid var(--colors-light-eaeaea-100, #EAEAEA);
+  border-radius: 1rem;
 }
-.topTextBoxTRA
-{
-position: absolute;
-width: 27rem;
-height: 10rem;
-flex-shrink: 0; 
-background: white;
-border: 2px solid var(--colors-light-eaeaea-100, #EAEAEA);
-border-radius: 1rem;
-transition:background-color 0.5s ease;
+
+.topTextBoxTRA {
+  position: absolute;
+  width: 27rem;
+  height: 10rem;
+  flex-shrink: 0;
+  background: white;
+  border: 2px solid var(--colors-light-eaeaea-100, #EAEAEA);
+  border-radius: 1rem;
+  transition: background-color 0.5s ease;
 }
-.topTextBoxTRA:hover
-{
-background-color: rgb(237, 224, 224);
+
+.topTextBoxTRA:hover {
+  background-color: rgb(237, 224, 224);
 }
-.scoreBox
-{
+
+.scoreBox {
   position: absolute;
   width: 5rem;
   height: 5rem;
-  flex-shrink: 0; 
+  flex-shrink: 0;
   background: white;
   border: 2px solid var(--colors-light-eaeaea-100, #EAEAEA);
   border-radius: 1rem;
   text-align: center;
-  
+
 }
-.recentGameBoxLeft
-{
+
+.recentGameBoxLeft {
   position: absolute;
   width: 20rem;
   height: 5rem;
-  flex-shrink: 0; 
+  flex-shrink: 0;
   background: white;
   border: 2px solid var(--colors-light-eaeaea-100, #EAEAEA);
   border-radius: 1rem;
-  transition:background-color 0.5s ease;
+  transition: background-color 0.5s ease;
 }
-.recentGameBoxLeft:hover
-{
-background-color: rgb(240, 240, 240);
+
+.recentGameBoxLeft:hover {
+  background-color: rgb(240, 240, 240);
 }
 
 
 /* 字体风格 */
-.textDate
-{
-position:absolute;
-top: -2.5rem;
-left: 2.7rem;
-color: var(--colors-text-dark-172239100, #172239);
-font-family: 'Britannic';
-font-size: 2.5rem;
-font-style: normal;
-font-weight: 300;
-line-height: normal; 
+.textDate {
+  position: absolute;
+  top: -2.5rem;
+  left: 2.7rem;
+  color: var(--colors-text-dark-172239100, #172239);
+  font-family: 'Britannic';
+  font-size: 2.5rem;
+  font-style: normal;
+  font-weight: 300;
+  line-height: normal;
 }
-.textStatus
-{
-  position:absolute;
+
+.textStatus {
+  position: absolute;
   color: var(--colors-text-dark-172239100, #0f41ac);
   font-family: '华文楷体';
   font-size: 1.5rem;
   top: 2.2rem;
   left: 11rem;
 }
-.textLeague
-{
-  position:absolute;
+
+.textLeague {
+  position: absolute;
   color: var(--colors-text-dark-172239100, black);
   font-family: '等线';
   font-size: 2rem;
   top: 5rem;
-  left:5rem;
+  left: 5rem;
   font-weight: 550;
 }
-.textTeamName
-{
-  position:absolute;
+
+.textTeamName {
+  position: absolute;
   color: var(--colors-text-dark-172239100, black);
   font-family: '仿宋';
   font-size: 2rem;
   top: -2rem;
   font-weight: 800;
 }
-.textScore
-{
+
+.textScore {
   font-family: Verdana;
   font-size: 1.5rem;
   color: var(--colors-text-dark-172239100, black);
   font-weight: 800;
 }
-.recentGameTypo
-{
+
+.recentGameTypo {
   font-family: Verdana;
   font-size: 1.3rem;
   color: var(--colors-text-dark-172239100, black);
   font-weight: 600;
   position: absolute;
-  top:17rem;
+  top: 17rem;
 }
-.textMatchDate
-{
+
+.textMatchDate {
   font-family: 'Britannic';
   font-size: 1rem;
   color: var(--colors-text-dark-172239100, black);
   font-weight: 600;
   position: absolute;
-  top:-0.7rem;
+  top: -0.7rem;
 }
-.textMatchScore
-{
+
+.textMatchScore {
   font-family: Georgia;
   font-size: 2rem;
   color: var(--colors-text-dark-172239100, black);
   font-weight: 600;
   position: absolute;
-  top:-0.7rem;
+  top: -0.7rem;
 }
-.textMatchOpp
-{
+
+.textMatchOpp {
   font-family: Verdana;
   font-size: 1.5rem;
   color: var(--colors-text-dark-172239100, black);
   font-weight: 600;
   position: absolute;
-  top:-0rem;
+  top: -0rem;
 }
 
-.picTeam
-{
+.picTeam {
   position: absolute;
   width: 6rem;
   height: 6rem;
-  top:3rem;
+  top: 3rem;
   object-fit: cover;
 }
-
-
 </style>
