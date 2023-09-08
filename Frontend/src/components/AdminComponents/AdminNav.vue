8076190132583b0a7e04150b48c5dfbdb5acfe80
Xie_yi <103160912+HeartFaker@users.noreply.github.com>
2023-09-08 21:35:36 +0800

    更新

--- Frontend/src/components/AdminComponents/AdminNav.vue
+++ Frontend/src/components/AdminComponents/AdminNav.vue
@@ -12,9 +12,6 @@
         logout() {
             localStorage.removeItem('token');
             this.$router.push('/');
-            setTimeout(() => {
-                window.location.reload(); // 刷新当前页面
-            }, 100); // 2000毫秒后刷新，你可以根据需要调整延迟时间
             return
         }
     }
